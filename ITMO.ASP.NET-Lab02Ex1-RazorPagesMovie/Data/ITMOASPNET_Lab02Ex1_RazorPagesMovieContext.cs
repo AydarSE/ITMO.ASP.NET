27821e189afcc0d0ee4cc605857f0829002131b6
AydarSE <Galiev_aidar@mail.ru>
2022-03-27 22:24:39 +0300

    Add Razor Ex2

---
+++ ITMO.ASP.NET-Lab02Ex1-RazorPagesMovie/Data/ITMOASPNET_Lab02Ex1_RazorPagesMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ITMO.ASP.NET_Lab02Ex1_RazorPagesMovie.Models;
+
+namespace ITMO.ASP.NET_Lab02Ex1_RazorPagesMovie.Data
+{
+    public class ITMOASPNET_Lab02Ex1_RazorPagesMovieContext : DbContext
+    {
+        public ITMOASPNET_Lab02Ex1_RazorPagesMovieContext (DbContextOptions<ITMOASPNET_Lab02Ex1_RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ITMO.ASP.NET_Lab02Ex1_RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ ITMO.ASP.NET-Lab02Ex1-RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ITMO.ASP.NET_Lab02Ex1_RazorPagesMovie.Data;
+using ITMO.ASP.NET_Lab02Ex1_RazorPagesMovie.Models;
+
+namespace ITMO.ASP.NET_Lab02Ex1_RazorPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ITMO.ASP.NET_Lab02Ex1_RazorPagesMovie.Data.ITMOASPNET_Lab02Ex1_RazorPagesMovieContext _context;
+
+        public CreateModel(ITMO.ASP.NET_Lab02Ex1_RazorPagesMovie.Data.ITMOASPNET_Lab02Ex1_RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
