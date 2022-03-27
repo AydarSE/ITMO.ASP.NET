f9e98262f3398438a5ff3cd9466a1e6f57fdd6b5
AydarSE <Galiev_aidar@mail.ru>
2022-03-27 20:47:34 +0300

    Add Razor Lab01Ex1-5

---
+++ ITMO.ASP.NET/ITMO.ASP.NET-L01Ex01-WebAppCoreProduct/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ITMO.ASP.NET_L01Ex01_WebAppCoreProduct.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
---
+++ ITMO.ASP.NET/ITMO.ASP.NET-L01Ex01-WebAppCoreProduct/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITMO.ASP.NET_L01Ex01_WebAppCoreProduct.Pages
+{
+    public class IndexModel : PageModel
+    {
+       public void OnGet()
+        {
+         
+        }
+
+    }
+}
