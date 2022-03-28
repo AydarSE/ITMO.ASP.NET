389313045ed9f944fe0177940a1b264b280d07e0
AydarSE <Galiev_aidar@mail.ru>
2022-03-28 11:45:04 +0300

    Add MVC Lab01,03

---
+++ ITMO.ASP.NET/ITMO.ASP.NET-MVC-L01Ex01-WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITMO.ASP.NET_MVC_L01Ex01_WebMVCR1.Models;
+
+namespace ITMO.ASP.NET_MVC_L01Ex01_WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*public string Index(string hel)
+        {
+            string Greeting = ModelClass.ModelHello() + ", " + hel;
+            return Greeting;
+        }*/
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+            return View(); 
+        }
+    }
+}
---
+++ ITMO.ASP.NET/ITMO.ASP.NET-MVC-L01Ex01-WebMVCR1/Models/ModelClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ITMO.ASP.NET_MVC_L01Ex01_WebMVCR1.Models
+{
+    public class ModelClass
+    { 
+        public static string ModelHello()
+        { 
+            int hour = DateTime.Now.Hour; 
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            return Greeting;
+        } 
+    }
+}
